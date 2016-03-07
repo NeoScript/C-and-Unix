8790d72fbe81961708789055904c5af10b5b5fa5
hameed67 <hameedshakimi@gmail.com>
2016-03-07 11:34:07 -0600

    Create FuelGauge.cpp

---
+++ program4/FuelGauge.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+#include "FuelGauge.h"
+
+void FuelGauge::printGallonsOfFuel()
+{
+    cout << "Gallons of fuel: " << gallonsOfFuel << endl;
+}
+
+void FuelGauge::addFuel()
+{
+    //  fuel tank is already full
+    if (gallonsOfFuel == 15)
+    {
+        cout << "Fuel Tank is full, cannot add more. " << endl;
+        return;
+    }
+    
+    //  adding a gallon of fuel fills up the tank
+    if (gallonsOfFuel + 1 == 15)
+    {
+        cout << "Adding one gallon to the tank. The tank is now full." << endl;
+        gallonsOfFuel++;
+        return;
+    }
+    
+    //  adding a gallon of fuel
+    else
+    {
+        cout << "Adding one gallon to the tank. ";
+        gallonsOfFuel++;
+        printGallonsOfFuel();
+    }
+}
+
+void FuelGauge::removeFuel()
+{
+    //  fuel tank is already empty
+    if (gallonsOfFuel == 0)
+    {
+        cout << "Fuel Tank is empty. Cannot remove any more. " << endl;
+        return;
+    }
+    
+    //  removing a gallon of fuel depletes the tank
+    if (gallonsOfFuel - 1 == 0)
+    {
+        cout << "Removing one gallon from the tank. The tank is now empty." << endl;
+        gallonsOfFuel--;
+        return;
+    }
+    
+    //  removing a gallon of fuel
+    else
+    {
+        cout << "Removing one gallon from the tank.";
+        gallonsOfFuel--;
+        printGallonsOfFuel();
+    }
+}
