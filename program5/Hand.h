12fd7779fc333fe2bef26dfd71d9f8722274b373
NeoScript <neoscripting@gmail.com>
2016-03-28 22:24:39 -0500

    Update Hand.h

--- program5/Hand.h
+++ program5/Hand.h
@@ -2,11 +2,12 @@
 // Created by nasir on 3/23/16.
 //
 /**
- *Filename:
- *Author:
+ *Filename: Hand.h
+ *Author: Nasir
  *Classes/Procedures:
- *		-Classes:
- *			1)Hand:
+ *	sortHand() -> sorts the hand for output in correct way
+ *	addCard() -> adds given card to Hand
+ *	getCardsOfSuit() -> returns vector of cards with specific suit
  */
 #ifndef PROGRAM5_HAND_H
 #define PROGRAM5_HAND_H
