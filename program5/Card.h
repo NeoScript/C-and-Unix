c26316a1b81b47d69ebfc2db58f3798a64c1125a
NeoScript <neoscripting@gmail.com>
2016-03-28 22:15:34 -0500

    comments for Card.h

--- program5/Card.h
+++ program5/Card.h
@@ -4,8 +4,10 @@
 
 /**
  *Filename:Card.h
- *Author:
- *Classes/Procedures:
+ *Author: Nasir
+ *Classes/Procedures: constructor creates a card with a value and a given suit
+ * 			getSuit returns a pointer to the suit
+ * 			get value returns the string value of the card
  *		Class:
  			1) Card : Contains the card's suit and value. Also has overloaded constructor and get-methods.
  */
