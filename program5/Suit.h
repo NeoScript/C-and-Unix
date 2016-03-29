c9d9b6b125469b4f1634d8bb97964dd55224d5bf
NeoScript <neoscripting@gmail.com>
2016-03-28 22:28:18 -0500

    Update Suit.h

--- program5/Suit.h
+++ program5/Suit.h
@@ -1,6 +1,10 @@
 //
 // Created by nasir on 3/28/16.
 //
+/**
+* this class is just used for inheritence
+* the faces all are children of the Suit class, and extend the virtual function in order to hold their Suit value
+*/
 
 #ifndef PROGRAM5_SUIT_H
 #define PROGRAM5_SUIT_H
