8db99f959bb818f191468603dc849a9a498f978d
NeoScript <neoscripting@gmail.com>
2016-03-30 15:28:10 -0500

    starting off stuff

--- program6/main.cpp
+++ program6/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
+#include <sys/socket.h>
 using namespace std;
 
+
 int main() {
+
+	int socketfd;
+
+	socketfd = socket(AF_INET, SOCK_STREAM, 0);
 	cout << "Hello, World!" << endl;
 	return 0;
 }
