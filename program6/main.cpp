5c15e2e7c4a88e126d0915abb5039fe65dc1ada0
NeoScript <neoscripting@gmail.com>
2016-04-18 16:02:22 -0500

    stuff probably happened

--- program6/main.cpp
+++ program6/main.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
-#include <sys/socket.h>
-using namespace std;
+#include <signal.h>
+#include <bits/socket.h>
+#include <netinet/in.h>
 
+int main(int argc, char **argv) {
 
-int main() {
+	int listenfd, connfd;
+	pid_t childpid;
+	socklen_t clientLength;
+	struct sockaddr_in cliaddr, servaddr;
 
-	int socketfd;
-
-	socketfd = socket(AF_INET, SOCK_STREAM, 0);
-	cout << "Hello, World!" << endl;
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
 	return 0;
 }
